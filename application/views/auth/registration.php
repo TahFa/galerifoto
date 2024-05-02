86a8b3f4263ec5a2e2cdb1050845f98d7bddbfbe
Tahfa <mbeeeeerrrrr16@gmail.com>
2024-05-02 12:09:26 +0700

    fix error auth2

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -6,8 +6,6 @@
     <title><?= $title; ?></title>
 </head>
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
 <body>
 
 <div class="container">
